7c9439d4974c07d1c1ba6b4fb1b710fff13a6229
Anirvan Chatterjee <57606+anirvan@users.noreply.github.com>
2020-01-03 12:20:27 -0800

    added code navigation comments for PDF section

--- yahoo-group-archive-tools.pl
+++ yahoo-group-archive-tools.pl
@@ -667,7 +667,11 @@ sub run {
     }
     $log->notice("[$list_name] wrote consolidated mailbox at $mbox_file");
 
+    # 8. Create PDF files
+
     if ($run_pdf) {
+
+        # 8.1 Create individual PDF files
 
         $log->notice(
             qq{[$list_name] Attempting to run experimental email2pdf conversion. If this fails immediately, try running "$email2pdf_path -h" and make sure it returns help text.}
@@ -697,6 +701,8 @@ sub run {
                 );
             }
         }
+
+        # 8.2 Create merged PDF file
 
     EachPdfFileToAppend:
         if (@pdf_files) {
